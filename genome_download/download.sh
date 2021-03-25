d81ad7324179daaed75eb766f6a410f4114acc0b
Finlay Maguire <finlaymaguire@gmail.com>
2021-03-25 17:55:07 -0300

    Tidy download script

--- genome_download/download.sh
+++ genome_download/download.sh
@@ -11,7 +11,7 @@ python scripts/get_ena_checksums.py accession_lists/gouliouris_2018_uk_accession
 mkdir -p gouliouris_2018_uk
 cd gouliouris_2018_uk
 perl ../scripts/sra_download.pl --ascp ../accession_lists/gouliouris_2018_uk_accessions.txt
-find -name '*.gz'  -exec md5sum {} >> ../checksums/gouliouris_2018_uk_downloaded +;
+find . -name '*.gz'  -exec md5sum {} \; >> ../checksums/gouliouris_2018_uk_downloaded
 cd ..
 python scripts/compare_checksums.py checksums/gouliouris_2018_uk_ena checksum_check/gouliouris_2018_uk_downloaded
 
@@ -19,19 +19,19 @@ python scripts/get_ena_checksums.py accession_lists/raven_2016_uk_hospital_acces
 mkdir -p raven_2016_uk_hospital
 cd raven_2016_uk_hospital
 perl ../scripts/sra_download.pl --ascp ../accession_lists/raven_2016_uk_hospital_accessions.txt
-find -name '*.gz'  -exec md5sum {} >> ../checksums/raven_2016_uk_hospital_downloaded +;
+find . -name '*.gz'  -exec md5sum {} \; >> ../checksums/raven_2016_uk_hospital_downloaded
 cd ..
 python scripts/compare_checksums.py checksums/raven_2016_uk_hospital_ena checksums/raven_2016_uk_hospital_downloaded
 
 mkdir -p zaheer_2020_alberta_sra
 cd zaheer_2020_alberta_sra
 prefetch --progress --verify yes --resume yes --output-directory . --option-file ../accession_lists/zaheer_2020_alberta_accessions.txt
-vdb-validate *
+vdb-validate ./*
 mkdir -p ../zaheer_2020_alberta
 for acc in *
     do 
-        mkdir -p ../ab_dataset_reads/$acc
-        fasterq-dump -p -O ../ab_dataset_reads/$acc --threads 4 $acc
-        pigz -p 4 ../ab_dataset_reads/$acc/*.fastq
+        mkdir -p ../ab_dataset_reads/"$acc"
+        fasterq-dump -p -O ../ab_dataset_reads/"$acc" --threads 4 "$acc"
+        pigz -p 4 ../ab_dataset_reads/"$acc"/*.fastq
 done
 rm -rf zaheer_2020_alberta_sra
